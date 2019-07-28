d5b1a39e06bf1dba6f2abf68d64fbab32579e617
Theron Jones <theronqj@gmail.com>
2019-07-28 12:56:02 -0700

    Initial Commit

---
+++ QuickPhotosTests/QuickPhotosTests.swift
@@ -0,0 +1,34 @@
+//
+//  QuickPhotosTests.swift
+//  QuickPhotosTests
+//
+//  Created by TJ on 7/28/19.
+//  Copyright © 2019 TJ. All rights reserved.
+//
+
+import XCTest
+@testable import QuickPhotos
+
+class QuickPhotosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
